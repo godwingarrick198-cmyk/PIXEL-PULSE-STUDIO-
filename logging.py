3f7b83fbf6e69fcda9462c41d22c7a70c36e2b2a
Garrick Godwin <godwingarrick198@gmail.com>
2026-08-25 13:26:24 +0100

    Delete logging.py

--- logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-logger = logging.getLogger("pixel_pulse")
-
-class EventLogger:
-    def event(self, name, **data):
-        logger.info("%s %s", name, data)
-
-events = EventLogger()
