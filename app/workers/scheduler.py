dec1c97ae665f539535dfb37e58d37ba1aec736c
Garrick Godwin <godwingarrick198@gmail.com>
2026-08-26 03:50:14 +0100

    Create scheduler worker

---
+++ app/workers/scheduler.py
@@ -0,0 +1,8 @@
+try:
+    from apscheduler.schedulers.asyncio import AsyncIOScheduler
+    scheduler=AsyncIOScheduler()
+except Exception:
+    class _NoopScheduler:
+        def start(self): pass
+        def shutdown(self,wait=False): pass
+    scheduler=_NoopScheduler()
