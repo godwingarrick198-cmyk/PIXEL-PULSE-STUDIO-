541ea62d9fe87d7cbe33435c446d5f1a6744f808
Garrick Godwin <godwingarrick198@gmail.com>
2026-08-28 13:00:43 +0100

    Register Apollo prospect provider

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,6 +1,7 @@
 from .base import ProspectProvider
+from .apollo import ApolloProvider
 from .osm import OSMProvider
 from .web import WebDiscoveryProvider
 from .product_hunt import ProductHuntProvider
 
-__all__ = ["ProspectProvider", "OSMProvider", "WebDiscoveryProvider", "ProductHuntProvider"]
+__all__ = ["ProspectProvider", "ApolloProvider", "OSMProvider", "WebDiscoveryProvider", "ProductHuntProvider"]
